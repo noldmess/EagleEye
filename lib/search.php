151ede5df61526bb7d353ba56492dbfc1c9180f3
noldmess <aaron.messner@student.uibk.ac.at>
2013-01-21 16:34:29 +0100

    search templet

---
+++ lib/search.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* ownCloud - bookmarks plugin
+*
+* @author David Iwanowitsch
+* @copyright 2012 David Iwanowitsch <david at unclouded dot de>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+class OC_Search_Provider_FaceFinder extends  OC_Search_Provider{
+function search($query) {
+	$searchResultarray=array();
+	$Initialisemodul=new OC_Module_Maneger();
+	$moduleclasses=$Initialisemodul->getModuleClass();
+	foreach ($moduleclasses as $moduleclass){
+		$searchResultarray=array_merge($searchResultarray,$moduleclass::search($query));
+		}
+		return $searchResultarray;
+	}
+}
---
+++ templates/search.php
@@ -0,0 +1,82 @@
+<?php
+if(isset($_GET['tag'])&&(isset($_GET['name']))){
+	echo "<h1>sdfsdf</h1>";
+	$seachResult=$_GET['search']::searchArry($_GET['name'],$_GET['tag']);
+	$result=count($seachResult);
+	$searchbool=true;
+}else{
+	$searchbool=false;
+}
+?>
+<div id="controls">
+<?php
+	if($searchbool){
+	echo '<h1>Suche nach:'.$_GET['tag'].' ergibt resultate '.$result.'</h1>';
+}else{
+	echo '<h1>Keine korekte suche </h1>';
+
+}
+?>
+</div>
+<div id="search">
+<?php
+//<a><img src="'+image.imagstmp+'"  alt="'+image.imagsname+'"></a>
+
+
+foreach ($seachResult as $img){
+	echo '<a><img src="'.\OCP\Util::linkTo('gallery', 'ajax/thumbnail.php').'?filepath='.urlencode($img).'" alt='.urlencode($img).'></a>';
+}
+?>
+</div>
+<div id="photoview">
+
+<div id="tool_left">
+<div id="photo">
+<div id="photoview_load"></div>
+<img alt="" src="">
+</div>
+<div id="tool_taggs">
+<div id="taggs"></div>
+<textarea></textarea></div>
+</div>
+<div id="tool_righte">
+<div class="tool">
+<div class="tool_title">
+<div class="tool_ico"></div>
+<h1>Modula A</h1>
+</div>
+<div class="tool_items">
+<p>Kamera:dsfsdfds</p>
+<p>Kamera:</p>
+<p>Kamera:</p>
+<p>Kamera:</p>
+</div>
+
+</div>
+<div class="tool">
+<div class="tool">
+<div class="tool_title">
+<div class="tool_ico"></div>
+<h1>Modula A</h1>
+</div>
+<div class="tool_items">dsgfsfg
+<p>Kamera:dsfsdfds</p>
+<p>Kamera:</p>
+<p>Kamera:</p>
+<p>Kamera:</p>
+</div>
+</div>
+<div class="tool">
+<div class="tool">
+<div class="tool_title">
+<div class="tool_ico"></div><h1>Modula A</h1>
+</div>
+</div>
+<div class="tool">
+<div class="tool">
+<div class="tool_title">
+<div class="tool_ico"></div><h1>Modula A</h1>
+</div>
+</div>
+</div>
+</div>
