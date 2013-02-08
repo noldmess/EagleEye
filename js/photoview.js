4892f2391793c6995aaeb2f625ebd3635d235579
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-08 11:52:52 +0100

    undo

--- js/photoview.js
+++ js/photoview.js
@@ -6,7 +6,7 @@ var PhotoView={
 		ClickImg:function(event){
 			//set PhotoView visible
 	 		$('#photo_load').show();
-			$('#photo img').attr("src", OC.linkTo('gallery', 'ajax/thumbnail.php')+'?img=/'+event.alt);
+			$('#photo img').attr("src", OC.linkTo('gallery', 'ajax/viewImage.php')+'?img=/'+event.alt);
 			$('#photo img').attr("name", event.alt);
 		
 			$('#photoview img').load(function(){
