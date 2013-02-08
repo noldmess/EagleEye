157266eae76afd4c226fed11424da67cc726acfc
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-08 11:51:30 +0100

    cange

--- js/photoview.js
+++ js/photoview.js
@@ -1,44 +1,27 @@
-$(document).ready(function(){
-	$('#photoview').hide();
-	//$('#photoview img').attr("src", 'http://localhost/owncloud/?app=gallery&getfile=ajax%2FviewImage.php?img=DSC_0019.jpg');//+this.alt);
-	$('#photoview_load').hide();
 
-
-
-$(' img ').click(function(event){
-				
-			$('#photoview_load').show();
-			$('#photoview img').attr("src", '/owncloud/?app=gallery&getfile=ajax%2FviewImage.php?img='+this.alt);
+var PhotoView={
+		load:function(){
+			$('#photoview').hide();
+		},
+		ClickImg:function(event){
+			//set PhotoView visible
+	 		$('#photo_load').show();
+			$('#photo img').attr("src", OC.linkTo('gallery', 'ajax/thumbnail.php')+'?img=/'+event.alt);
+			$('#photo img').attr("name", event.alt);
+		
 			$('#photoview img').load(function(){
 				$('#photoview_load').hide();
 			});
 			
+			/**
+			 * @todo hier modul js für eingehöngte js
+			 */
+			Module.load(event.alt);
+			
 			$('#photoview img').ready(function(){
-				$('#photo p').remove(":contains('dsfsdfsdfsd')");
 				$('#photoview img').show();
 			});
-			$('#search').hide();
+			$('#new_1').hide();
 			$('#photoview').show();
-			
-			
-		
-
-});
-$('div.tool_title').click(function(){
-	if( $(this).parent().children("div.tool_items").is(":visible")){
-		$(this).parent().children("div.tool_items").slideUp(500);
-	}else{
-		$(this).parent().children("div.tool_items").slideDown(500);
-	}
-});
-
-$(document).keypress(function(e) {
-  //alert(e.keyCode);
-   if ( e.keyCode== 27){
-      $('#photoview').hide();
-      $('#search').show();
-   }
-});
-
-
-});
+		}
+};
