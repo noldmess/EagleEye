4b3fa2e4bc06db83d0a8ed57fb6f421cf2497534
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-11 17:24:07 +0100

    update to new owncloud version

--- templates/search.php
+++ templates/search.php
@@ -23,60 +23,59 @@ if(isset($_GET['search'])&&isset($_GET['tag'])&&(isset($_GET['name']))){
 
 
 foreach ($seachResult as $img){
-	echo '<a><img src="'.\OCP\Util::linkTo('gallery', 'ajax/thumbnail.php').'?filepath='.$img.'" alt='.$img.'></a>';
+	echo '<a><img src="'.\OCP\Util::linkTo('gallery', 'ajax/thumbnail.php').'?file='.\OCP\USER::getUser().$img.'" alt='.$img.'></a>';
 	
 }
 ?>
 </div>
 <div id="photoview">
 
-<div id="tool_left">
-<div id="photo">
-<div id="photoview_load"></div>
-<img alt="" src="">
-</div>
-<div id="tool_taggs">
-<div id="taggs"></div>
-<textarea></textarea></div>
-</div>
-<div id="tool_righte">
-<div class="tool">
-<div class="tool_title">
-<div class="tool_ico"></div>
-<h1>Modula A</h1>
-</div>
-<div class="tool_items">
-<p>Kamera:dsfsdfds</p>
-<p>Kamera:</p>
-<p>Kamera:</p>
-<p>Kamera:</p>
-</div>
-
-</div>
-<div class="tool">
-<div class="tool">
-<div class="tool_title">
-<div class="tool_ico"></div>
-<h1>Modula A</h1>
-</div>
-<div class="tool_items">dsgfsfg
-<p>Kamera:dsfsdfds</p>
-<p>Kamera:</p>
-<p>Kamera:</p>
-<p>Kamera:</p>
-</div>
-</div>
-<div class="tool">
-<div class="tool">
-<div class="tool_title">
-<div class="tool_ico"></div><h1>Modula A</h1>
-</div>
-</div>
-<div class="tool">
-<div class="tool">
-<div class="tool_title">
-<div class="tool_ico"></div><h1>Modula A</h1>
-</div>
-</div>
-</div>
+	<div id="tool_left">
+		<div id="photo" >
+			<!-- <img id="img_img"alt="" src=""> -->
+		</div>
+		<div id="tool_taggs">
+		<div id="taggs"></div>
+		<textarea></textarea></div>
+	</div>
+	<div id="tool_righte">
+			<div class="tool Exif">
+				<div class="tool_title">
+					<div class="tool_ico"></div>
+					<h1>Exif</h1>
+				</div>
+				<div class="tool_items"></div>
+		</div>
+	<!--	<div class="tool">
+			<div class="tool_title">
+				<div class="tool_ico"></div>
+				<h1>GPS</h1>
+			</div>
+				<div class="tool_items">
+					  <div id="demoMap" style="height:200px;width:auto;"></div>
+					<script src="http://www.openlayers.org/api/OpenLayers.js"></script>
+					<script>
+						 var lat            = 47.35387;
+					    var lon            = 8.43609;
+					    var zoom           = 18;
+					 
+					    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+					    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+					    var position       = new OpenLayers.LonLat(lon, lat).transform( fromProjection, toProjection);
+					 
+					    map = new OpenLayers.Map("demoMap");
+					    var mapnik         = new OpenLayers.Layer.OSM();
+					    map.addLayer(mapnik);
+					 
+					    var markers = new OpenLayers.Layer.Markers( "Markers" );
+					    map.addLayer(markers);
+					    markers.addMarker(new OpenLayers.Marker(position));
+					 
+					    map.setCenter(position, zoom);
+					</script>
+				</div>
+			</div>
+		</div>-->
+		</div>
+	</div>
 </div>
