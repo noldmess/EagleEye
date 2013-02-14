32f19f505aceb83c5ac711f9730b516a8f469960
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-14 10:23:20 +0100

    Update templates/equivalent.php

--- templates/equivalent.php
+++ templates/equivalent.php
@@ -22,12 +22,14 @@ foreach ($arrayAllEquivalent as $n=>$s){
 	echo "Array".json_encode($arrayAllEquivalent)."<br>";
 	foreach ($s['equival'] as  $dubb){
 		foreach ($module as $index=>$array_modul){
-			echo $index." ".json_encode($array_modul)."<br>";
-			if(isset($array_modul[$dubb])){
-				echo $dubb."<br>";
-				$arrayAllEquivalent[$n]['equival'] +=array($dubb);//$array_modul[$dubb]['equival'];
-				 $arrayAllEquivalent[$n]['value']+=$array_modul[$dubb]['value'];
-				unset($module[$index][$dubb]);
+			foreach($array_modul[1] as $array){
+				echo $index." ".json_encode($array)."<br>";
+				if(isset($array_modul[$dubb])){
+					echo $dubb."<br>";
+					$arrayAllEquivalent[$n]['equival'] +=array($dubb);//$array_modul[$dubb]['equival'];
+				 	$arrayAllEquivalent[$n]['value']+=$array_modul[$dubb]['value'];
+					unset($module[$index][$dubb]);
+				}
 			}
 		}
 	}
