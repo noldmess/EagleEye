3f7944309c7ff0a03142ccfd8e1e44fd7a04296f
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-14 11:06:21 +0100

    Update templates/equivalent.php

--- templates/equivalent.php
+++ templates/equivalent.php
@@ -28,7 +28,7 @@ foreach ($arrayAllEquivalent as $n=>$s){
 				echo $dubb."=>".json_encode($array['equival'])."<br>";
 				$key=array_search($dubb,$array['equival']);
 				echo $key."<br>";
-				if($key!=false){
+				if(!$key){
 					echo $dubb."=".$photo."->".$key."<br>";
 					$arrayAllEquivalent[$n]['equival'] +=array($photo);//$array_modul[$dubb]['equival'];
 				 	$arrayAllEquivalent[$n]['value']+=$array_modul[$dubb]['value'];
