7414ca83fcf8c7e80999faab405372077b4f41f7
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-23 19:51:44 +0100

    Update ajax/ equivalent.php

--- ajax/ equivalent.php
+++ ajax/ equivalent.php
@@ -1,18 +1,3 @@
 <?php
-echo "dsfdsfdsfs";
-/*OCP\JSON::checkLoggedIn();
-OCP\JSON::checkAppEnabled('facefinder');
-$photo= new OC_FaceFinder_Photo("");
-$Initialisemodul=new OC_Module_Maneger();
-$moduleclasses=$Initialisemodul->getModuleClass();
-$photoArray=$photo->equivalent();
-foreach ($moduleclasses as $moduleclass){
-  $moduleopject=new $moduleclass("");
-  $moduleArray[]=$moduleopject->equivalent();
-}
-$photo=OC_Equivalent_Result::equalety($photoArray,$moduleArray);
-foreach($photo as $PhotoName=>$photoArray){
-  $help=arsort($photoArray);
-}
-echo json_encode($photo);*/
-?>
+echo "dsfdsfdsfs";?>
+
