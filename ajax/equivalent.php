c2339ba8e788e8d927ebb82ef49961a91d33f962
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-23 19:53:23 +0100

    Create equivalent.php

---
+++ ajax/equivalent.php
@@ -0,0 +1,18 @@
+
+<?php
+OCP\JSON::checkLoggedIn();
+OCP\JSON::checkAppEnabled('facefinder');
+$photo= new OC_FaceFinder_Photo("");
+$Initialisemodul=new OC_Module_Maneger();
+$moduleclasses=$Initialisemodul->getModuleClass();
+$photoArray=$photo->equivalent();
+foreach ($moduleclasses as $moduleclass){
+  $moduleopject=new $moduleclass("");
+  $moduleArray[]=$moduleopject->equivalent();
+}
+$photo=OC_Equivalent_Result::equalety($photoArray,$moduleArray);
+foreach($photo as $PhotoName=>$photoArray){
+  $help=arsort($photoArray);
+}
+echo json_encode($photo);
+?>
