745a14d89a37787f9681947b6dcae4c757707d3e
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-24 08:53:59 +0100

    Update js/equivalent.js

--- js/equivalent.js
+++ js/equivalent.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	$.getJSON(OC.linkTo('facefinder', 'ajax/equivalent.php'), function(data) {
 		$.each(data,function(index_year,data){
+			$("equivalent_test").append('<div class="equival"><div class="equival_value">Equal</div></div>');
 			alert(data.img);
 			$.each(data.array,function(index_year,data){
 				alert(data.img_eq+"-"+data.value);
