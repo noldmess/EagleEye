6f15c75233ba264441a7a4d3ef3dd8ab74d81845
noldmess <aaron.messner@student.uibk.ac.at>
2013-02-24 08:48:35 +0100

    Update js/equivalent.js

--- js/equivalent.js
+++ js/equivalent.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	$.getJSON(OC.linkTo('facefinder', 'ajax/equivalent.php'), function(data) {
 		$.each(data,function(index_year,data){
+			alert(data.img);
 			$("equivalent_test").append('<div class="year"></div>');
 		});
 	});
