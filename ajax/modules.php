d1c306fa39903bca0825d82d8bf3a843dc2d8f93
aaron <aaron@ubuntu-aaron>
2013-02-27 14:12:25 +0100

    final modue

--- ajax/modules.php
+++ ajax/modules.php
@@ -1,3 +1,15 @@
 <?php
-echo json_encode(array("Tag","Exif","Kamera"));
+OCP\JSON::checkLoggedIn();
+OCP\JSON::checkAppEnabled('facefinder');
+$scriptArray=array();
+$Initialisemodul=new OC_Module_Maneger();
+$moduleclasses=$Initialisemodul->getModuleClass();
+
+foreach ($moduleclasses as $moduleclass){
+	$arrayScript=$moduleclass::getArrayOfScript();
+	foreach($arrayScript as $script){
+		$scriptArray[]=$script;
+	}
+}
+echo json_encode($scriptArray);
 ?>
