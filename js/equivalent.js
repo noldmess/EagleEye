2f899132950f811572b8fe0dda97f6ff4b1bc0e1
aaron <aaron@ubuntu-aaron>
2013-03-02 11:23:02 +0100

    new method to delete images

--- js/equivalent.js
+++ js/equivalent.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+	$("#controls").append('<span class="right"><a title="Remove" ><button class="remove">Remove</button></a></span>');
+	$("button.remove").click(function(){
+		var s=$("img[checked]");
+		$.each(s,function(index_yeaffr,data){
+			  // $.getJSON(OC.linkTo('facefinder', 'ajax/removeequivalent.php')+"?img="+data.img_eq, function(data) {}); 
+			if($(data).val()==="1"){
+				
+			//alert($(data).attr("alt"));
+			 var div_equal=$(data).parent().parent();
+			  var equival=$(div_equal).parent();
+			if($(div_equal).find("a ").length==1){
+				$(div_equal).remove();
+			}
+			if($(equival).find("div.equival_photos").length==0){
+			 $(equival).remove();
+			}
+			
+			$(data).parent().remove();
+			//  $( this ).dialog( "close" );
+			}
+		});
+		});
 	$('#equivalent').addClass('loading');
 	$.getJSON(OC.linkTo('facefinder', 'ajax/equivalent.php'), function(data) {
 
@@ -7,18 +29,29 @@ $(document).ready(function() {
 			$("#equivalent").append('<div class="equival"><div class="equival_value">Equal</div><img src="'+OC.linkTo('gallery', 'ajax/thumbnail.php')+'?file='+oc_current_user+'/'+data.img+'" alt="'+data.img+'"></div>');
 			var tmp_value=1000;
 			var help=0;
+			
 			$.each(data.array,function(index_yeaffr,data){
 				  if(tmp_value>data.value){
 					  help++;
 					  tmp_value=data.value;
 					  $("#equivalent div.equival:eq("+index_year+")").append('<div class="equival_photos"><div class="equival_value">'+tmp_value+'</div></div>');
 				  }
-				  $("#equivalent div.equival:eq("+index_year+") div.equival_photos:eq("+(help-1)+") ").append('<a><img src="'+OC.linkTo('gallery', 'ajax/thumbnail.php')+'?file='+oc_current_user+'/'+data.img_eq+'" alt="'+data.img_eq+'"></a>');
-					  $("#equivalent div.equival:eq("+index_year+") div.equival_photos:eq("+(help-1)+") a img").click(function(){
-						  var img=this;
+				  
+				  $("#equivalent div.equival:eq("+index_year+") div.equival_photos:eq("+(help-1)+") ").append('<a><img checked="checked" src="'+OC.linkTo('gallery', 'ajax/thumbnail.php')+'?file='+oc_current_user+'/'+data.img_eq+'" alt="'+data.img_eq+'"></a>');
+					  $("#equivalent div.equival:eq("+index_year+") div.equival_photos a img:eq("+index_yeaffr+")").click(function(){;
+							 if($(this).val()!=="1"){
+							// $(this).hide();
+							 $(this).addClass('remove');
+							 $(this).val("1")
+							 }else{
+								 $(this).removeClass('remove');
+								 $(this).val("0") 
+							 }
+						  /*var img=this;
 						  $( "#dialog-confirm" ).attr('title', "Remove Photo"+data.img_eq);
-						  Dialog(img);
+						  Dialog(img);*/
 					  });
+
 			});
 			
 		});
