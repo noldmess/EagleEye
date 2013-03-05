34b2c39e15136950874f6af6547f3d9ff7954500
aaron <aaron@ubuntu-aaron>
2013-03-05 08:46:44 +0100

    df

--- ajax/removeequivalent.php
+++ ajax/removeequivalent.php
@@ -5,9 +5,9 @@ $user = \OCP\USER::getUser();
 $tmp=new OC_FaceFinder_Photo($_GET['img']);
 $tmp->remove();
 $galleryDir = \OC_User::getHome($user).'/files';
-$thumbPath = $galleryDir . $_GET['img'];
-if (file_exists($thumbPath)) {
-	unlink($thumbPath);
+$thumbPath = $_GET['img'];
+if (\OC\Files\Filesystem::file_exists($thumbPath)) {
+	\OC\Files\Filesystem::unlink($thumbPath);
 }else{
 	echo "$thumbPath";
 }
