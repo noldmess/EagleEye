705a68ba1eb6abad18aa602586f8566a7004bad7
aaron <aaron@ubuntu-aaron>
2013-04-10 10:24:21 +0200

    fix bug

--- ajax/equivalent.php
+++ ajax/equivalent.php
@@ -2,11 +2,11 @@
 OCP\JSON::checkLoggedIn();
 OCP\JSON::checkAppEnabled('facefinder');
 $photo= new OC_FaceFinder_Photo("");
-$Initialisemodul==OC_Module_Maneger::getInstance();
-$moduleclasses=$Initialisemodul->getModuleClass();
+$writemodul=OC_Module_Maneger::getInstance();
+$moduleclasses=$writemodul->getModuleClass();
 $photoArray=$photo->equivalent();
 foreach ($moduleclasses as $moduleclass){
-  $moduleopject=new $moduleclass("");
+  $moduleopject=new $moduleclass['Mapper']("");
   $moduleArray[]=$moduleopject->equivalent();
 }
 $photo=OC_Equivalent_Result::Ajaxequalety($photoArray,$moduleArray);
