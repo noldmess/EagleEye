ddfd211beaa4008ee719f31b34b184f8c095907b
aaron <aaron@ubuntu-aaron>
2013-05-21 12:58:22 +0200

    dsf

--- ajax/PhotoByData.php
+++ ajax/PhotoByData.php
@@ -1,10 +1,10 @@
 <?php
 OCP\JSON::checkLoggedIn();
 OCP\JSON::checkAppEnabled('facefinder');
-$year=2013;
-$month=5;
-$day=15;
-$dir="/";
+$year=(int)$_GET['year'];
+$month=(int)$_GET['month'];
+$day=(int)$_GET['day'];
+$dir==$_GET['dir'];
 $sql="SELECT path,photo_id FROM `*PREFIX*facefinder` Where uid_owner LIKE ? ";
 $sqlvar=array(\OCP\USER::getUser());
 
@@ -12,23 +12,24 @@ if(strlen($dir)==!0){
 	$sql.=' AND path like ? ';
 	$sqlvar[]=$dir."%";
 }
-if(strlen($year)==!0){
+if($year==!0){
 	$sql.=' AND YEAR(date_photo) = ? ';
 	$sqlvar[]=$year;
 }
-if(strlen($month)==!0){
+if($month==!0){
 	$sql.=' AND month(date_photo) = ? ';
 	$sqlvar[]=$month;
 }
-if(strlen($day)==!0){
+if($day==!0){
 	$sql.=' AND Day(date_photo) = ? ';
 	$sqlvar[]=$day;
 }
+
 $sql.='order by date_photo ASC';
 $stmta = \OCP\DB::prepare($sql);
-echo $sql;
-echo json_encode($sqlvar);
 $result = $stmta->execute($sqlvar);
+$images=array();
 while (($row = $result->fetchRow())!= false) {
-	echo $row['path']."<br>";
+	$images[]=array("imagsid"=>$row['photo_id'],"imagsname"=>$row['path']);
 }
+echo OCP\JSON::success(array('data'=>$images));
