9695e7975db647ac1d29ab1414a30ce7d4a98f7a
aaron <aaron@ubuntu-aaron>
2013-05-27 08:05:02 +0200

    sdf

--- templates/index.php
+++ templates/index.php
@@ -32,74 +32,15 @@ if($size>1)
  
  <div id="photoff">
 	 <div id="tool_right">
-	 <ul  class="start">
-		 <li>2013
-			 <ul>
-			 <li>Januar</li>
-			 <li>Februar</li>
-			 <li>April</li>
-			 <li>Juni</li>
-			 <li>Juli
-				 <ul>
-				 <li>Juli.1</li>
-				 <li>Juli.2</li>
-				 <li>Juli.3</li>
-				 <li>Juli.4</li>
-				 <li>Juli.5</li>
-				 </ul>
-			 </li>
-			 </ul>
-		 </li>
-	 </ul>
+		 <ul class="start"></ul>
 	 </div>
 	 <div id="photoOverView"></div>
 </div>
-
 <div id="photoview">
-
 	<div id="tool_left">
-		<div id="photo" >
-			<!-- <img id="img_img"alt="" src=""> -->
-		</div>
-		<div id="tool_taggs">
-			
-		</div>
+		<div id="photo" ></div>
 	</div>
-	<div id="tool_righte">
-			
-	
-	<!--	<div class="tool">
-			<div class="tool_title">
-				<div class="tool_ico"></div>
-				<h1>GPS</h1>
-			</div>
-				<div class="tool_items">
-					  <div id="demoMap" style="height:200px;width:auto;"></div>
-					<script src="http://www.openlayers.org/api/OpenLayers.js"></script>
-					<script>
-						 var lat            = 47.35387;
-					    var lon            = 8.43609;
-					    var zoom           = 18;
-					 
-					    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-					    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-					    var position       = new OpenLayers.LonLat(lon, lat).transform( fromProjection, toProjection);
-					 
-					    map = new OpenLayers.Map("demoMap");
-					    var mapnik         = new OpenLayers.Layer.OSM();
-					    map.addLayer(mapnik);
-					 
-					    var markers = new OpenLayers.Layer.Markers( "Markers" );
-					    map.addLayer(markers);
-					    markers.addMarker(new OpenLayers.Marker(position));
-					 
-					    map.setCenter(position, zoom);
-					</script>
-				</div>
-			</div>
-		</div>-->
-		</div>
-	</div>
+	<div id="tool_righte"></div>
 </div>
 
 
