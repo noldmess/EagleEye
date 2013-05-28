9132660b08ca71f42e7d6e91f56aaf30204e3d0e
aaron <aaron@ubuntu-aaron>
2013-05-28 09:56:43 +0200

    sdfsd

--- module/js/tag.js
+++ module/js/tag.js
@@ -1,86 +1,120 @@
 $(document).ready(function() {
-	$("#tool_righte").append('<div class="tool Tag"><div class="tool_title"><i class="icon-info-sign"></i>Tag<input   type="checkbox"  value="" name="query" ></input></div><div class="tool_items"><ul></ul></div><div class="tool_items fix"><input   type="text"  value="" name="query" ></input><input type="submit" value=" Set Tag "></input></div></div>');
-	$("span.right").append('<button class="tag" style=""> Tag </button>');
+	tag.init();
+});
+
+
+
+function tag(){	
+		this.load=function(image){
+			//remove old tags from view
+			$("#tool_righte .tool.Tag .tool_items ul *").remove();
+			$("#photo div").remove();
+			$("#tool_taggs textarea").val("");
+			//load tags from the image
+			tag.getTag(image);
+			$('#photo img').click(function(e){
+				tag.maketag(e);
+				});
+		};
+};
+
+
+tag.init=function(){
+	$("#tool_righte").append('<div class="tool Tag"><div class="tool_title"><i class="icon-info-sign"></i>Tag</div><div class="tool_items"><ul></ul></div><div class="tool_items fix"><input   type="text"  value="" name="query"  placeholder="Write Tag"></input><input type="submit" value=" Set Tag "></input><p><label for="male">All tags Visible</label><input   type="checkbox"  value="" name="query" ></input></p></div></div>');
+	$("span.right").append('<button class="tag" style=""> Tag </button><input   type="text"  value="" name="query"  placeholder="Write Tag"></input><input type="submit" value=" Set Tag "></input>');
 	$("button.tag").click(function(e){
 		tag.click();
 	});
 	
 	$("#tool_righte .tool.Tag .tool_items input[type='submit']").click(function(e){
-		alert("Sdfsdf");
+		tag.key();
 	});
 	$("#tool_righte .tool.Tag .tool_items input[type='text']").keyup(function(e){
-		tag.key(e,this);
+		if ( e.keyCode== 13){
+			tag.key();
+		}
 	});
-});
-function tag(){	
-		this.load=function(image){
-			$("#tool_righte .tool.Tag .tool_items ul *").remove();
-			$("#photo div").remove();
-			$("#tool_taggs textarea").val("");
-			tag.getTag(image);
-			$('#photo img').click(function(e){
-				  var PosX = 0;
-				  var PosY = 0;
-				  var posX = 0;
-				  var posY = 0;
-				  Eltern = document.getElementById("img_img")
-				  while (Eltern) {
-					  posX += Eltern.offsetLeft;
-				      posY += Eltern.offsetTop;
-					  Eltern = Eltern.offsetParent;
-					}
-				  if (e.clientX || e.clientY)
-				    {
-				      PosX = e.clientX + document.body.scrollLeft
-				        + document.documentElement.scrollLeft;
-				      PosY = e.clientY + document.body.scrollTop
-				        + document.documentElement.scrollTop;
-				    }
-				 PosX = PosX - posX;
-				 PosY = PosY - posY;
-				// alert(PosX+' '+PosY);
-				 //for ckorekt  possiton in the photoview div 
-				 PosX+=document.getElementById("img_img").offsetLeft;
-				 PosY+=document.getElementById("img_img").offsetTop;//
-				 $("#photo").append('<div class="draggable" style="position: absolute; top: '+(PosY-50)+'px; left: '+(PosX-50)+'px;"><input   type="text"  value="" name="query" ></input></div>');
-				 $("#photo .draggable input").keyup(function(e){
-						//Tag.key(e);
-					 
-					 if ( e.keyCode== 13){
-						 var pos=findPosition(this.parentNode);
-						 var image=$('#photoview img').attr("alt");
-						 var tag_name=$(this).val();
-						 var x1=(pos[0]/document.getElementById("img_img").offsetWidth);
-				 		 var y1=(pos[1]/document.getElementById("img_img").offsetHeight);
-				 		 var x2=($(this).parent().width()/document.getElementById("img_img").offsetWidth);
-				 		 var y2=($(this).parent().height()/document.getElementById("img_img").offsetHeight);
-							$.getJSON(OC.linkTo('facefinder', 'ajax/inserttagposition.php')+"?image="+image+"&tag="+tag_name+"&x1="+x1+"&x2="+x2+"&y1="+y1+"&y2="+y2, function(data) {
-								$("#tool_righte .tool.Tag .tool_items ul *").remove();
-								$("#photo div").remove(".draggable");
-								tag.getTag(image);
-							});
-						
-							/*$(this).parent().append('<div class="draggable_tag">'+tag+'</div>');
-							$(this).parent().draggable({ disabled: true });
-							$(this).parent().attr('class', 'draggable_fix');
-							$("#photo .draggable_fix").click(function (e){
-							$(this).draggable({ disabled: false });
-							$(this).attr('class', 'draggable');
-						});*/
-						
-						$(this).parent().remove(".draggable_fix");
-						
-					 }
-					});
-				
-				 $('#photo .draggable').draggable({
-					    cursor: 'move',
-					    containment: '#img_img'
-					  } ).resizable();
-				
+	
+	$("span.right input[type='submit']").click(function(e){
+		tag.checkbox();
+	});
+	$("span.right input[type='text']").keyup(function(e){
+		if ( e.keyCode== 13){
+			tag.checkbox();
+		}
+	});
+	
+	$("#tool_righte .tool.Tag .tool_items input[type='checkbox']").click(function(e){
+		if($("#photo div.tag_in_photo").is(':visible')){
+			$("#photo div.tag_in_photo").hide();
+		}else{
+			$("#photo div.tag_in_photo").show();
+		}
+	});
+}
+
+tag.maketag=function(e){
+	 var PosX = 0;
+	  var PosY = 0;
+	  var posX = 0;
+	  var posY = 0;
+	  Eltern = document.getElementById("img_img")
+	  while (Eltern) {
+		  posX += Eltern.offsetLeft;
+	      posY += Eltern.offsetTop;
+		  Eltern = Eltern.offsetParent;
+		}
+	  if (e.clientX || e.clientY)
+	    {
+	      PosX = e.clientX + document.body.scrollLeft
+	        + document.documentElement.scrollLeft;
+	      PosY = e.clientY + document.body.scrollTop
+	        + document.documentElement.scrollTop;
+	    }
+	 PosX = PosX - posX;
+	 PosY = PosY - posY;
+	// alert(PosX+' '+PosY);
+	 //for ckorekt  possiton in the photoview div 
+	 PosX+=document.getElementById("img_img").offsetLeft;
+	 PosY+=document.getElementById("img_img").offsetTop;//
+	 $("#photo").append('<div class="draggable" style="position: absolute; top: '+(PosY-50)+'px; left: '+(PosX-50)+'px;"><input   type="text"  value="" name="query" ></input></div>');
+	 $("#photo .draggable input").keyup(function(e){
+			//Tag.key(e);
+		 
+		 if ( e.keyCode== 13){
+			 var pos=findPosition(this.parentNode);
+			 var image=$('#photoview img').attr("alt");
+			 var tag_name=$(this).val();
+			 var x1=(pos[0]/document.getElementById("img_img").offsetWidth);
+	 		 var y1=(pos[1]/document.getElementById("img_img").offsetHeight);
+	 		 var x2=($(this).parent().width()/document.getElementById("img_img").offsetWidth);
+	 		 var y2=($(this).parent().height()/document.getElementById("img_img").offsetHeight);
+				$.getJSON(OC.linkTo('facefinder', 'ajax/inserttagposition.php')+"?image="+image+"&tag="+tag_name+"&x1="+x1+"&x2="+x2+"&y1="+y1+"&y2="+y2, function(data) {
+					$("#tool_righte .tool.Tag .tool_items ul *").remove();
+					$("#photo div").remove(".draggable");
+					tag.getTag(image);
 				});
-		};
-};
+			
+				/*$(this).parent().append('<div class="draggable_tag">'+tag+'</div>');
+				$(this).parent().draggable({ disabled: true });
+				$(this).parent().attr('class', 'draggable_fix');
+				$("#photo .draggable_fix").click(function (e){
+				$(this).draggable({ disabled: false });
+				$(this).attr('class', 'draggable');
+			});*/
+			
+			$(this).parent().remove(".draggable_fix");
+			
+		 }
+		});
+	
+	 $('#photo .draggable').draggable({
+		    cursor: 'move',
+		    containment: '#img_img'
+		  } ).resizable();
+	
+}
+
 
 tag.getTag=function(img){
 	$.getJSON(OC.linkTo('facefinder', 'ajax/tag.php')+'?image='+img, function(data) {
@@ -90,15 +124,12 @@ tag.getTag=function(img){
 				if(data.x1>0 || data.x2>0 || data.y1>0 || data.y2>0){
 					 var x=(parseFloat(data.x1)*document.getElementById("img_img").offsetWidth);
 			 		 var y=(parseFloat(data.y1)*document.getElementById("img_img").offsetHeight);
-			 		 var x2=(parseFloat(data.x2)*document.getElementById("img_img").offsetWidth-x);
-			 		 var y2=(parseFloat(data.y2)*document.getElementById("img_img").offsetHeight-y);
+			 		 var x2=(parseFloat(data.x2)*document.getElementById("img_img").offsetWidth);
+			 		 var y2=(parseFloat(data.y2)*document.getElementById("img_img").offsetHeight);
 					 var y1=(document.getElementById("img_img").offsetTop+y);
 					 var x1=(document.getElementById("img_img").offsetLeft+x);
 					$("#photo").append('<div id="'+data.name+' '+data.tag+'" class="tag_in_photo"style="position: absolute; top: '+y1+'px; left: '+x1+'px;"><div class="draggable_fix" style="width: '+x2+'px; height:'+y2+'px;"></div><div class="draggable_tag">'+data.tag+'</div></div>');
 					$('#photo div.tag_in_photo i').click(function(){
-						tag.removeTagDiv(this);
-					});
-					$('#photo div.draggable_fix i').click(function(){
 						tag.removeTagDiv(this);
 					});
 					extra='<i class="icon-search"></i>';
@@ -128,6 +159,9 @@ tag.removeTag=function(tagDiv){
 	var image=$('#photo img').attr("alt");
 	var tag=$(tagDiv).attr("name");
 	 $(tagDiv).parent().remove();
+	 if($("div[id='"+tag+"']")){
+		 $("div[id^='"+tag+"']").remove();
+	 }
 	 $.getJSON(OC.linkTo('facefinder', 'ajax/removetag.php')+"?image="+image+"&tag="+tag, function(data) {});
 };
 
@@ -140,6 +174,8 @@ tag.removeTagDiv=function(tagDiv){
 	 $.getJSON(OC.linkTo('facefinder', 'ajax/removetag.php')+"?image="+image+"&tag="+tag+"&imagepaht="+imagepaht, function(data) {});
 };
 
+
+//cange to tag Photo Over View
 tag.click=function(e){
 	var path=tag.getPath();
 	 $.getJSON(OC.linkTo('facefinder', 'ajax/allImagesTags.php')+'?dir='+path, function(data) {
@@ -165,17 +201,16 @@ tag.getPath=function(){
 	return path
 }
 
-
+//load tags for Photo Over View
 tag.loadImagesTag=function(tagelement){
 	var path=tag.getPath();
-	tagelement=tagelement.id;
 	 $('#tool_right *').removeClass('active');
-	 $('#tool_right ul li[id^="'+tagelement+'"]').addClass('active');
-	 $.getJSON(OC.linkTo('facefinder', 'ajax/allImagesTags.php')+'?dir='+path+'&tag='+tagelement, function(data) {
+	 var dsdf=$('#tool_right ul li[id="'+tagelement.id+'"]');
+	 $('#tool_right ul li[id="'+tagelement.id+'"]').addClass('active');
+	 $.getJSON(OC.linkTo('facefinder', 'ajax/allImagesTags.php')+'?dir='+path+'&tag='+tagelement.id, function(data) {
 		 $('#photoOverView').addClass('loading');
 		 $('#photoOverView * ').remove();
 		 if (data!==null && data.status == 'success'){
-			 	var t= data.photo
 			 	tag.photoOverView(data.photo);	
 	     }
 		 $('#photoOverView').removeClass('loading');
@@ -193,7 +228,7 @@ tag.sidebar=function(tags){
 
 tag.photoOverView=function(photos){
 	$.each(photos,function(index_tag,image){
-		 $('#photoOverView').append('<a name="'+image.path+'"><img src="'+OC.linkTo('gallery', 'ajax/thumbnail.php')+'?file='+oc_current_user+'/'+image.path+'"  alt="'+image.id+'"></a><input type="checkbox" original-title=""></input>');
+		 $('#photoOverView').append('<a name="'+image.path+'"><img src="'+OC.linkTo('gallery', 'ajax/thumbnail.php')+'?file='+oc_current_user+'/'+image.path+'"  alt="'+image.id+'"></a><input type="checkbox" original-title="" alt="'+image.id+'" ></input>');
 		 $('#photoOverView a[name^="'+image.path+'"] img').click(function(){
 	 			PhotoView.ClickImg(this)
 	 	});
@@ -202,10 +237,9 @@ tag.photoOverView=function(photos){
 
 
 
-tag.key=function(e,t){
-	if ( e.keyCode== 13){
-		var tag_value=$(t).val();
-		$(t).val('');
+tag.key=function(){
+		var tag_value=$("#tool_righte .tool.Tag .tool_items input[type='text']").val();
+		$("#tool_righte .tool.Tag .tool_items input[type='text']").val('');
 		var image=$('#photoview img').attr("alt");
 		$.getJSON(OC.linkTo('facefinder', 'ajax/inserttag.php')+"?image="+image+"&tag="+tag_value, function(data) {
 				$("#tool_righte .tool.Tag .tool_items ul *").remove();
@@ -213,7 +247,22 @@ tag.key=function(e,t){
 				tag.getTag(image);
 		});
  
+};
+
+tag.checkbox=function(){
+	var list=$('#photoOverView input[type="checkbox"]');
+	var tag_value=$("span.right input[type='text']").val();
+	if(tag_value.length>0){
+		$("span.right input[type='text']").val('');
+		var image=$('#photoview img').attr("alt");
+		$.each(list,function(index_tag,input){
+			if($(input).attr('checked')==='checked'){
+				var asdfdsaf=$(input).attr("alt");
+				$.getJSON(OC.linkTo('facefinder', 'ajax/inserttag.php')+"?image="+$(input).attr("alt")+"&tag="+tag_value, function(data) {});
+			}
+		});
 	}
+
 };
 
 
