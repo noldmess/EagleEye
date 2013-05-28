3dae96784e8f9e0fc8bae5c45d0bc144eba237c9
aaron <aaron@ubuntu-aaron>
2013-05-28 08:38:21 +0200

    sdf

--- js/test.js
+++ js/test.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	if(typeof Gallery!=='undefined'){
 		$('.right').append('<button class="facefinder" style="display: none;">facefinder</button><a class="share" data-possible-permissions="31" title="Teilen" data-item="" data-item-type="gallery"></a>');
 		 	$('button.facefinder').click(function(){
-		 		window.location = "index.php/"+OC.linkTo('facefinder', '/')+'?dir='+Gallery.currentAlbum;
+		 		var test="index.php/"+OC.linkTo('facefinder', '/')+'?dir='+Gallery.currentAlbum;
+		 		window.location = test;
 	   });
 		 $('button.facefinder').show();
 	}
