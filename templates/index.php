aa835bf3d7cd22a83e7bcbe093299e48656ece23
aaron <aaron@ubuntu-aaron>
2013-05-28 09:54:02 +0200

    dsf

--- templates/index.php
+++ templates/index.php
@@ -1,6 +1,6 @@
 <div id="controls">
 <div class="crumb ui-droppable" title='/'>
-	<a href="<?php echo \OCP\Util::linkTo('facefinder', '/')."?dir=/"?>"><img class="svg" src="/owncloud/core/img/places/home.svg"></img></a>
+	<a href="<?php echo \OCP\Util::linkTo('facefinder', 'index.php')."?dir=/"?>"><img class="svg" src="/owncloud/core/img/places/home.svg"></img></a>
 </div>
 <?php 
 $size=sizeof($_['patharray']);
@@ -13,11 +13,11 @@ if($size>1)
 	if($size-$count===0){
 ?>
 <div class="crumb last svg" title="<?php echo $path?>">
-	<a href="<?php echo \OCP\Util::linkTo('facefinder', '/')."?dir=".$path?>"><?php echo $a?></a>
+	<a href="<?php echo \OCP\Util::linkTo('facefinder', 'index.php')."?dir=".$path?>"><?php echo $a?></a>
 </div>
 <?php }else{?>
 <div class="crumb  svg ui-droppable" title="<?php echo $path?>">
-	<a href="<?php echo \OCP\Util::linkTo('facefinder', '/')."?dir=".$path?>""><?php echo $a?></a>
+	<a href="<?php echo \OCP\Util::linkTo('facefinder', 'index.php')."?dir=".$path?>""><?php echo $a?></a>
 </div>
 <?php
 }
