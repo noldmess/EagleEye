71e63bba5d15f2ecad1f6c5e391c37b14347251b
aaron <aaron@ubuntu-aaron>
2013-05-30 17:42:22 +0200

    go to facefinder from data

--- js/add.js
+++ js/add.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 	if(typeof Gallery!=='undefined'){
 		$('.right').append('<button class="facefinder" style="display: none;">facefinder</button><a class="share" data-possible-permissions="31" title="Teilen" data-item="" data-item-type="gallery"></a>');
 		 	$('button.facefinder').click(function(){
-		 		window.location = OC.linkTo('facefinder', 'index.php')+'?dir='+Gallery.currentAlbum;
+		 		window.location = OC.linkTo('facefinder', 'index.php')+'?dir='+encodeURIComponent(Gallery.currentAlbum).replace(/%2F/g, '/');
 	   });
 		 $('button.facefinder').show();
 	}
 	
 	if (typeof FileActions !== 'undefined') {
-		FileActions.register('dir', 'facefinder', OC.PERMISSION_DELETE, function () {
+		FileActions.register('dir', 'Facefinder', OC.PERMISSION_DELETE, function () {
 			//image for action 
-			return OC.imagePath('core', 'places/picture.svg');
+		return "";//OC.imagePath('core', 'places/picture.svg');
 		}, function (filename) {
 			//action 
 			window.location = OC.linkTo('facefinder', 'index.php')+'?dir='+ encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+ '/' + encodeURIComponent(filename);
