8405827c6ec9dd8e49f0d98e6fa19dd176de053f
aaron <aaron@ubuntu-aaron>
2013-07-20 16:25:27 +0200

    dsfsd

--- ajax/equivalent.php
+++ ajax/equivalent.php
@@ -10,6 +10,8 @@ use OCA\FaceFinder;
 OCP\JSON::checkLoggedIn();
 OCP\JSON::checkAppEnabled('facefinder');
 $dir=$_GET['dir'];
+$page=(int)$_GET['page'];
+$pagesize=20;//(int)$_GET['page'];
 $writemodul=OCA\FaceFinder\ModuleManeger::getInstance();
 $moduleclasses=$writemodul->getModuleClass();
 $photo= new OCA\FaceFinder\FaceFinderPhoto("");
@@ -20,12 +22,11 @@ foreach ($moduleclasses as $moduleclass){
    $moduleArray+=array($moduleclass['Mapper']=>$moduleopject->equivalent($dir));	
 }
 for($i=0;$i<sizeof($photoArray);$i++){
-	foreach($moduleArray as $sss=>$module){
+	foreach($moduleArray as $module){
 		foreach($module as $image){
-			if($photoArray[$i][0]["photo_id"]===$image[0]["photo_id"] && $photoArray[$i][1]["photo_id"]===$image[1]["photo_id"] ||$photoArray[$i][1]["photo_id"]===$image[0]["photo_id"] && $photoArray[$i][0]["photo_id"]===$image[1]["photo_id"]){
+			if(($photoArray[$i][0]["photo_id"]===$image[0]["photo_id"] && $photoArray[$i][1]["photo_id"]===$image[1]["photo_id"] )||($photoArray[$i][1]["photo_id"]===$image[0]["photo_id"] && $photoArray[$i][0]["photo_id"]===$image[1]["photo_id"])){
 				$photoArray[$i]['prozent']*=0.80;
 				$photoArray[$i]['prozent']+=$image['prozent']*0.2;
-			//	echo $photoArray[$i]['prozent']." ".($photoArray[$i]['prozent']*0.99)." $sss ".$image['prozent']."\n";
 				break;
 			}
 		}
@@ -33,6 +34,8 @@ for($i=0;$i<sizeof($photoArray);$i++){
 	}
 }
 usort($photoArray,  "test");
-$photoArray=array_slice($photoArray,0,20);
-echo OCP\JSON::success(array("data"=>$photoArray));
+$sizeArray=sizeof($photoArray);
+
+$photoArray=array_slice($photoArray,$page*$pagesize,$pagesize);
+echo OCP\JSON::success(array("data"=>$photoArray,"size"=>$sizeArray));
 ?>
