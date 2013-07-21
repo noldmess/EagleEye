7438aaee2975e5f483a8b74903ecab3a496e5412
aaron <aaron@ubuntu-aaron>
2013-07-21 08:46:06 +0200

    asdfghj

--- module/FaceDetaction_Module.php
+++ module/FaceDetaction_Module.php
@@ -129,7 +129,7 @@ class FaceDetaction_Module implements OCA\FaceFinder\MapperInterface{
 						$class->addTag("2#025",$tag['tag'],$section['x1'],$section['y1'],$section['y1'],$section['y2']);
 						Tag_Module::update($class);
 						OCP\Util::writeLog("facefinder","found",OCP\Util::DEBUG);
-						self::insertFacePhoto($tag['tag_id'],$newClass,$imgToSava."-".$facecount.".png",$face['class']$section['y1'],$section['x2'],$section['y2']);
+						self::insertFacePhoto($tag['tag_id'],$newClass,$imgToSava."-".$facecount.".png",$face['class'],$section['x1'],$section['y1'],$section['x2'],$section['y2']);
 					}else{
 						OCP\Util::writeLog("facefinder","not found",OCP\Util::DEBUG);
 						self::insertFacePhoto(null,$newClass,$imgToSava."-".$facecount.".png",null,$section['x1'],$section['y1'],$section['x2'],$section['y2']);
