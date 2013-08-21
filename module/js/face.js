bb95c07fedb03575f44a9188f6a05f682159e63b
aaron <aaron@ubuntu-aaron>
2013-08-21 09:06:15 +0200

    asfadsf

--- module/js/face.js
+++ module/js/face.js
@@ -99,7 +99,7 @@ face.getTag=function(img){
 				var addTagDiv=$(this).parent().children('.addTag');
 				if($(addTagDiv).is(":visible") !== true){
 					$(addTagDiv).show();
-					$(addTagDiv).css("z-index","100");
+					$(addTagDiv).css("z-index","105");
 				}
 			});
 		/*$('#photo .draggable_face_2').mouseleave(function() {
