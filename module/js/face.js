c4ddad05ac04fc8ea81f97fec902a624089e7099
aaron <aaron@ubuntu-aaron>
2013-08-21 16:05:31 +0200

    dfdsf

--- module/js/face.js
+++ module/js/face.js
@@ -55,37 +55,14 @@ face.getTag=function(img){
 						// $("#photo").append('<div class="face outer "style="position: absolute; top: '+(y1+(y2-y1))+'px; left: '+x1+'px;" id="'+index_tag+'"><div class="face inner" style=" width: '+(y2-y1)+'px; height:'+(y2-y1)+'px;  position: absolute; top: '+y1+'px; left: '+x1+'px;""></div><div class="fram"><i class="icon-remove-sign" alt="'+data.face_id+'" src="'+OC.linkTo('facefinder', 'img/delete.png')+'" name="'+data.name+' '+data.tag+'"></i><input   type="text"  value=""></input></div></div>');
 						 //$("#photo").append('<div class="draggable_face" style="position: absolute; top: '+(PosY)+'px; left: '+(PosX-115+((y2-PosY)/2))+'px;  width:230px;"><div class="draggable_face_2" style=" width: '+(y2-PosY)+'px; height:'+(y2-PosY)+'px;"><a id="fancybox-close" style="display: inline;"><i class="icon-remove"></i></a></div><div class="addTag"><input id="'+data.face_id+'"  alt="'+data.x1+'-'+data.y1+'-'+(data.x2-data.x1)+'-'+(data.y2-data.y1)+'" type="text"  value="" name="query" placeholder="add Face Name" ></input><input type="button" value=" Set Tag "></input></div  ></div>');
 						 $("#photo").append('<div class="draggable_face" style="position: absolute; top: '+(PosY)+'px; left: '+(PosX-115+((y2-PosY)/2))+'px;" ><div class="draggable_face_2" style="position: absolute;  left: '+(115-(y2-PosY)/2)+'px;   width: '+(y2-PosY)+'px; height:'+(y2-PosY)+'px;"><a id="fancybox-close" ><i class="icon-remove"></i></a></div><div class="addTag" style=" position: absolute;  top:'+(y2-PosY)+'px;"><input id="'+data.face_id+'"  alt="'+data.x1+'-'+data.y1+'-'+(data.x2-data.x1)+'-'+(data.y2-data.y1)+'" type="text"  value="" name="query" placeholder="add Face Name" ></input><input type="button" value=" Set Tag "></input></div  ></div>');
-							/* $('#photo input[id="'+data.face_id+'"]').keyup(function(e){
-								 if ( e.keyCode== 13){
-									 //e.delegateTarget.alt
-									var test=$(this).parent().parent();
-									 var tag_name=$(this).val();
-									 var image=$('#photoview img').attr("alt");
-									 var face_id=$(this).attr("id");
-									 var pos=$(this).attr("alt");
-									 OC.Notification.show("Updating face Data Set");
-									 $('html').css( 'cursor', 'wait' );
-									 $.getJSON(OC.linkTo('facefinder', 'ajax/faceinsert.php')+"?image="+image+"&tag="+tag_name+"&face_id="+face_id+"&pos="+pos, function(data) {
-										 $("#photo div.draggable_face").remove();
-										 $("#photo div.tag_in_photo").remove();
-										 $("#tool_righte .tool.Face .tool_items table *").remove();
-										 $("#tool_righte .tool.Key .tool_items table *").remove();
-										 $("#tool_righte .tool.Tag .tool_items table *").remove();
-										 setTimeout(function(){
-											OC.Notification.hide();
-											 face.getTag(image);
-											 tag.getTag(image);
-											 $(test).remove();
-										}, 300);
-
-										
-									 });*/
-								 }
 						 });
 				
 					//$('#tool_righte .tool.Face .tool_items tbody').append('<tr><td>Not Set Face</td></tr>');
 				}
 			//});
+			$('#photo .draggable_face .draggable_face_2 a').click(function(e){
+				alert("sdfsdf");
+			 });
 			$('#photo .draggable_face .addTag input[type="text"]').keyup(function(e){
 				if ( e.keyCode== 13){
 					face.setFaceInImage(this,$(this).val(),$(this).attr("id"),$(this).attr("alt"));
