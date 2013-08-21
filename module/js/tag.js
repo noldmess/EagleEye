0bf5650f670244fba83295f921644f99e3430ef3
aaron <aaron@ubuntu-aaron>
2013-08-21 07:46:31 +0200

    sdfsd

--- module/js/tag.js
+++ module/js/tag.js
@@ -51,10 +51,10 @@ function tag(){
 		};
 		this.resat=function(){
 			$("#moduleFildsinner input[name='query']").text("Set Tag (0)");
-			var test=$("#moduleFildsinner button.submitTag );
+			var test=$	q("#moduleFildsinner button.submitTag");
 			var test=$("#moduleFildsinner input[name='counterTag']");
 			$("#moduleFildsinner input[name='counterTag']").attr("value","0");
-			$("#moduleFildsinner button.submitTag ).attr("value","0");
+			$("#moduleFildsinner button.submitTag").attr("value","0");
 		};
 		this.setEvents=function(){
 			 $('#photoOverView input[type="checkbox"]').click(function(event){
