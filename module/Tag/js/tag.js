a64debc16ba8e250eb859fb5cd717ed4e61a3e5e
aaron <aaron@ubuntu-aaron>
2013-09-24 09:24:53 +0200

    sfgf

--- module/Tag/js/tag.js
+++ module/Tag/js/tag.js
@@ -11,7 +11,7 @@ function tag(){
 			$("#tool_taggs textarea").val("");
 			//load tags from the image
 			tag.getTag(image);
-			var sdfsdfr=$('#photo img');
+			var sfdfsdfr=$('#photo img');
 			$('#photo').click(function(e){
 				tag.maketag(e);
 			});
