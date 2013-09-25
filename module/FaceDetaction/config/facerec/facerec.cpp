9ce53d837a0ad5d73a3128ccc56dd328f53e34c6
aaron <aaron@ubuntu-aaron>
2013-09-25 16:13:32 +0200

    c++

---
+++ module/FaceDetaction/config/facerec/facerec.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2011. Philipp Wagner <bytefish[at]gmx[dot]de>.
+ * Released to public domain under terms of the BSD Simplified license.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the organization nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ *   See <http://www.opensource.org/licenses/bsd-license>
+ */
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace cv;
+using namespace std;
+
+static Mat norm_0_255(InputArray _src) {
+    Mat src = _src.getMat();
+    // Create and return normalized image:
+    Mat dst;
+    switch(src.channels()) {
+    case 1:
+        cv::normalize(_src, dst, 0, 255, NORM_MINMAX, CV_8UC1);
+        break;
+    case 3:
+        cv::normalize(_src, dst, 0, 255, NORM_MINMAX, CV_8UC3);
+        break;
+    default:
+        src.copyTo(dst);
+        break;
+    }
+    return dst;
+}
+
+static int read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+    int max=0;
+    std::ifstream file(filename.c_str(), ifstream::in);
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given filename.";
+        CV_Error(CV_StsBadArg, error_message);
+    }
+    string line, path, classlabel;
+    while (getline(file, line)) {
+        stringstream liness(line);
+        getline(liness, path, separator);
+        getline(liness, classlabel);
+        if(!path.empty() && !classlabel.empty()) {
+            images.push_back(imread(path, 0));
+            labels.push_back(atoi(classlabel.c_str()));
+	    if(atoi(classlabel.c_str())>=max)
+	       max=atoi(classlabel.c_str());
+        }
+    }
+    return max;
+}
+
+int main(int argc, const char *argv[]) {
+    // Check for valid command line arguments, print usage
+    // if no arguments were given.
+    if (argc < 2) {
+        cout << "usage: " << argv[0] << " <csv.ext> <output_folder> " << endl;
+        exit(1);
+    }
+    //string output_folder;
+  /*  if (argc == 3) {
+        output_folder = string(argv[2]);
+    }*/
+    // Get the path to your CSV.
+    //string fn_csv = string(argv[1]);
+    // These vectors hold the images and corresponding labels.
+    vector<Mat> images;
+    vector<int> labels;
+    int max;
+    // Read in the data. This can fail if no valid
+    // input filename is given.
+    /*try {
+        max=read_csv(fn_csv, images, labels);
+    } catch (cv::Exception& e) {
+        cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
+        // nothing more we can do
+        exit(1);
+    }*/
+    // Quit if there are not enough images for this demo.
+
+    // Get the height from the first image. We'll need this
+    // later in code to reshape the images to their original
+    // size:
+  //  int height = images[0].rows;
+    // The following lines simply get the last images from
+    // your dataset and remove it from the vector. This is
+    // done, so that the training data (which we learn the
+    // cv::FaceRecognizer on) and the test data we test
+    // the model with, do not overlap.
+    Mat testSample = imread(argv[2], 0);//images[images.size() - 1];
+  /*  int testLabel = labels[labels.size()-1];
+    images.pop_back();
+    labels.pop_back();*/
+    // The following lines create an Eigenfaces model for
+    // face recognition and train it with the images and
+    // labels read from the given CSV file.
+    // This here is a full PCA, if you just want to keep
+    // 10 principal components (read Eigenfaces), then call
+    // the factory method like this:
+    //
+    //      cv::createEigenFaceRecognizer(10);
+    //
+    // If you want to create a FaceRecognizer with a
+    // confidence threshold (e.g. 123.0), call it with:
+    //
+    //      cv::createEigenFaceRecognizer(10, 123.0);
+    //
+    // If you want to use _all_ Eigenfaces and have a threshold,
+    // then call the method like this:
+    //
+    //      cv::createEigenFaceRecognizer(0, 123.0);
+    //
+	Ptr<FaceRecognizer> model =createLBPHFaceRecognizer(1,8,8,8);
+	    model->load(argv[1]);
+	int predictedLabel = -1;
+	double confidence = 0.0;
+	model->predict(testSample, predictedLabel, confidence);
+	string result_message = format("Predicted class = %d / Actual class = %d.", predictedLabel);
+	cout <<predictedLabel<<" " << endl;
+
+    return 0;
+}
