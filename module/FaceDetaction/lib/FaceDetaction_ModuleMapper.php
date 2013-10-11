017fbb0e507511ef778a47f35caa7ceca675d8d3
aaron <aaron@ubuntu-aaron>
2013-10-11 22:41:36 +0200

    ydfsdgf

--- module/FaceDetaction/lib/FaceDetaction_ModuleMapper.php
+++ module/FaceDetaction/lib/FaceDetaction_ModuleMapper.php
@@ -276,9 +276,9 @@ public static function doBackgroundJob($array){
 	
 	public static  function issetFacePhotoIsNull($class,$facePhotoPath,$x1,$x2,$y1,$y2){
 		//if(!self::issetTagPhotoId($class->getForingkey(),$id)){
-		$stmt = OCP\DB::prepare('SELECT *  FROM  `*PREFIX*facefinder_facedetaction_face_photo_module` WHERE `photo_id` = ? and `facePhotoPath`= ? and x1 = ? and x2 = ? and y1 = ? and y2 = ? and faceclass is NULL');
+		$stmt = OCP\DB::prepare('SELECT *  FROM  `*PREFIX*facefinder_facedetaction_face_photo_module` WHERE `photo_id` = ? and `facePhotoPath`= ? and  faceclass is NULL');
 		//$stmt = OCP\DB::prepare('SELECT *  FROM `*PREFIX*facefinder_tag_photo_module` WHERE `photo_id`  = ? and `tag_id` = ?');
-		$result=$stmt->execute(array($class->getForingkey(),$facePhotoPath,$x1,$x2,$y1,$y2));
+		$result=$stmt->execute(array($class->getForingkey(),$facePhotoPath));
 		return ($result->numRows()==1);
 	}
 
