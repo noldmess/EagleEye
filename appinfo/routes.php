fec29389b13ce8a67f5aaf844db91b646fc56b8e
aaron <aaron@ubuntu-aaron>
2013-10-11 10:53:28 +0200

    awfasdfa

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,8 +1,8 @@
 <?php
 
-// this route matches /index.php/yourapp/myurl/SOMEVALUE
-$this->create('yourappname_routename', '/myurl/{key}')->action(
-    function($params){
-        require __DIR__ . '/../index.php';
-    }
+$this->create('facefinder', '/')->action(
+		function($params){
+			require __DIR__ . '/../index.php';
+		}
 );
+
