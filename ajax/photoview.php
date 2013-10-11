fe965e27f39a48e10bb0f5b889e1f8156cda84d8
aaron <aaron@ubuntu-aaron>
2013-10-11 15:17:55 +0200

    sadasd

--- ajax/photoview.php
+++ ajax/photoview.php
@@ -7,7 +7,7 @@ $id=(int)$_GET['id'];
 if($id>0){
 	$writemodul=OCA\FaceFinder\ModuleManeger::getInstance();
 	$photo=OCA\FaceFinder\FaceFinderPhoto::getPhotoClass($_GET['id']);
-	echo OCP\JSON::success(array('data'=>$photo->getJSON(),OCP\Util::linkToAbsolute ("EagleEye","/module/FaceDetaction/config/install")));
+	echo OCP\JSON::success(array('data'=>$photo->getJSON(),OC_App::getAppPath('EagleEye')."/module/FaceDetaction/config/install"));
 }else{
 		OCP\JSON::error(array("message"=>"get image must be an intager"));
 }
