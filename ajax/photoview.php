7966b6b61a813e151878eaacafeeb1b12f6c35aa
aaron <aaron@ubuntu-aaron>
2013-10-11 15:18:43 +0200

    dsfsdf

--- ajax/photoview.php
+++ ajax/photoview.php
@@ -4,10 +4,11 @@ OCP\JSON::checkLoggedIn();
 OCP\JSON::callCheck();
 OCP\JSON::checkAppEnabled('EagleEye');
 $id=(int)$_GET['id'];
+echo OC_App::getAppPath('EagleEye')."/module/FaceDetaction/config/install";
 if($id>0){
 	$writemodul=OCA\FaceFinder\ModuleManeger::getInstance();
 	$photo=OCA\FaceFinder\FaceFinderPhoto::getPhotoClass($_GET['id']);
-	echo OCP\JSON::success(array('data'=>$photo->getJSON(),OC_App::getAppPath('EagleEye')."/module/FaceDetaction/config/install"));
+	echo OCP\JSON::success(array('data'=>$photo->getJSON()));
 }else{
 		OCP\JSON::error(array("message"=>"get image must be an intager"));
 }
