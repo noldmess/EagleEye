89ba2e897fcb4c39d5b58b9ca27294559c1d3be7
noldmess <aaron.messner@student.uibk.ac.at>
2013-10-11 10:46:17 +0200

    Delete routes.php
    asdf

--- appinfo/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// this route matches /index.php/yourapp/myurl/SOMEVALUE
-$this->create('yourappname_routename', '/myurl/{key}')->action(
-    function($params){
-        require __DIR__ . '/../index.php';
-    }
-);
