1ab26e7aa75e9577cf06c8f1285da3a83c788d15
aaron <aaron@ubuntu-aaron>
2013-10-13 13:44:09 +0200

    sdfsdf

--- js/add.js
+++ js/add.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
 		return OC.imagePath('EagleEye', 'EagleEye.png');
 		}, function (filename) {
 			//action 
-			var d=encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+encodeURIComponent(filename).substr(1);
-			alert(encodeURIComponent($('#dir').val()).replace(/%2F/g, '/'));
+			var d=encodeURIComponent($('#dir').val()).replace(/%2F/g, '/').substr(1)+encodeURIComponent(filename).substr(1);
+			alert(encodeURIComponent($('#dir').val()).replace(/%2F/g, '/').substr(1));
 			alert(encodeURIComponent(filename).substr(1));
 			alert(d);
 			var params = {dir: d };
