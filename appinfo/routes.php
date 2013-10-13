1f793a6d225de17b8ce8f391f299a406b47e71ca
aaron <aaron@ubuntu-aaron>
2013-10-14 00:14:37 +0200

    sdfsdf

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -6,7 +6,8 @@ $this->create('EagleEyeView', '/{dir}')->action(
     function($params){
         require __DIR__ . '/../index.php';
     }
-)->create('EagleEyeSearch', '/Search/{search}/{tag}/{name}')->action(
+);
+$this->create('EagleEyeSearch', '/Search/{search}/{tag}/{name}')->action(
 		function($params){
 			require __DIR__ . '/../index.php';
 		}
