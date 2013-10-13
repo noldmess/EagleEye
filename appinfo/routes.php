ca7c6bb743e1cf21578dfca4c6eedec8cd19b5eb
aaron <aaron@ubuntu-aaron>
2013-10-13 23:51:52 +0200

    sdfdf

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,9 +2,9 @@
 
 <?php
 
-$this->create('EagleEye', '/{type}-{dir}/')->action(
+$this->create('EagleEye', '/{type}/{dir}/')->action(
     function($params){
         require __DIR__ . '/../index.php';
     }
-);
+)->defaults('type' => 'View');
 
