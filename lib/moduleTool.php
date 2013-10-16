fba35017430c7e7ee0ea0e2557f361478c253547
aaron <aaron@ubuntu-aaron>
2013-10-16 16:25:09 +0200

    test moule tool Class

---
+++ lib/moduleTool.php
@@ -0,0 +1,60 @@
+<?php
+namespace  OCA\FaceFinder;
+/**
+ * The class OC_Module_Maneger instal and include the module automatically
+ * ownCloud - faceFinder application
+ *
+ * @author Aaron Messner
+ * @copyright 2012 Aaron Messner aaron.messner@stuudent.uibk.ac.at
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.	If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+use OCP;
+class ModuleTool {
+	private $title;
+	private $itamList;
+	
+	public function   __construct($title) {
+		$this->title=$title;
+	}
+	
+	public function addFixItems(){
+		array_push($this->itamList,'<div class="tool_items fix" style=""></div>');
+	}
+	
+	public function addscrollItems(){
+		array_push($this->itamList,'<div class="tool_items" style=""></div>');
+	}
+	
+	public function buildModuleTool(){
+		$html='<div class="tool '.$this->title.'>';
+		$html.=getTitleDiv();
+		foreach($this->itemList as $item){
+			$html.=$item;
+		}
+		$html.='</div>';
+	}
+	
+	private function getTitleDiv(){
+		$titlediv='<div class="tool_title">'.
+						'<i class="icon-white icon-arrow-up"></i>'.
+						$this->title.
+				'</div>';
+		return $titlediv;
+	}
+	
+}
+
---
+++ module/Exif/ajax/modulTool.php
@@ -0,0 +1,8 @@
+<?php
+use OCA\FaceFinder;
+OCP\JSON::checkLoggedIn();
+OCP\JSON::callCheck();
+$tool=new ModuleTool("Exif");
+$tool->addscrollItems();
+echo $tool.buildModuleTool();
+?>
