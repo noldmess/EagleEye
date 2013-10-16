3c0a9655dd96e8fc9c7ddeedb185d9646ea5fb87
aaron <aaron@ubuntu-aaron>
2013-10-16 16:31:56 +0200

    test string

--- module/Exif/ajax/modulTool.php
+++ module/Exif/ajax/modulTool.php
@@ -2,6 +2,7 @@
 use OCA\FaceFinder;
 OCP\JSON::checkLoggedIn();
 OCP\JSON::callCheck();
+echo "dfssdf";
 $tool=new OCA\FaceFinder\ModuleTool("Exif");
 $tool->addscrollItems();
 echo $tool->buildModuleTool();
