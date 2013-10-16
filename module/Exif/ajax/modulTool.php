2d133de26b80ac3cfa17f358cdc521faed0be6f0
aaron <aaron@ubuntu-aaron>
2013-10-16 16:41:47 +0200

    dsfsdf

--- module/Exif/ajax/modulTool.php
+++ module/Exif/ajax/modulTool.php
@@ -3,7 +3,6 @@ use OCA\FaceFinder;
 OCP\JSON::checkLoggedIn();
 OCP\JSON::callCheck();
 OCP\JSON::checkAppEnabled('EagleEye');
-
 $tool=new OCA\FaceFinder\ModuleTool("Exif");
 $tool->addScrollItems();
 echo $tool->buildModuleTool();
