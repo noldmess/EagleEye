35c47e907d455ec1aacfaf10d4006a52c54103d4
aaron <aaron@ubuntu-aaron>
2013-10-16 16:35:56 +0200

    remove test string

--- module/Exif/ajax/modulTool.php
+++ module/Exif/ajax/modulTool.php
@@ -2,7 +2,8 @@
 use OCA\FaceFinder;
 OCP\JSON::checkLoggedIn();
 OCP\JSON::callCheck();
-echo "dfssdf";
+OCP\JSON::checkAppEnabled('EagleEye');
+
 $tool=new OCA\FaceFinder\ModuleTool("Exif");
 $tool->addscrollItems();
 echo $tool->buildModuleTool();
